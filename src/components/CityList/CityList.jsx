c39497b82bc2bd76fe2c69e92208a3358757bc93
mserhatyuksel <mserhat.yksl@gmail.com>
2021-10-31 21:31:37 +0300

    Added components

---
+++ src/components/CityList/CityList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./CityList.module.css";
+const CityList = () => {
+    return <div></div>;
+};
+
+export default CityList;
---
+++ src/components/Weather/Weather.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Weather.module.css";
+
+const Weather = () => {
+    return <div></div>;
+};
+
+export default Weather;
